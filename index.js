028d262096674e7ff4186019b1cdd69363709ee8
Jere Suikkila <jere@suikkila.com>
2019-10-12 11:47:45 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const uuidv4 = require("uuid/v4");
+
+app.get("/", (req, res) => res.json({ uuid: uuidv4() }));
+
+app.get("/:count", (req, res) => {
+  const count = parseInt(req.params.count);
+  if (count < 1) {
+    res.status(500).json({ status: 500, message: "Wrong number" });
+  }
+  const uuids = [];
+  for (let i = 0; i < count; i++) {
+    uuids.push(uuidv4());
+  }
+  res.json({ count, uuids });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
